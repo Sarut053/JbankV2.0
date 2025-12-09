c0ae5dcb98530172d77cf011acfa2b58c82cbf49
1CEsoni1 <samui-jack@hotmail.com>
2025-12-09 14:25:01 +0700

    fix ปัญหาเวอร์ชั่นให้รันได้

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DashboardView from '../views/DashboardView.vue'
 import AddExpenseView from '../views/AddExpenseView.vue'
 import ScanBillView from '../views/ScanBillView.vue'
 import NotificationView from '../views/NotificationView.vue'
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/dashboard', name: 'dashboard', component: DashboardView },
   { path: '/add', name: 'add', component: AddExpenseView },
